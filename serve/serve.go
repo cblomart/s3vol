4dcabcef50a27156ab7a270819995b65deb5ad3d
Cedric Blomart <cblomart@gmail.com>
2020-05-14 22:48:48 +0200

    remove socket on stop

--- serve/serve.go
+++ serve/serve.go
@@ -1,6 +1,8 @@
 package serve
 
 import (
+	"os"
+
 	"github.com/cblomart/s3vol/driver"
 	"github.com/docker/go-plugins-helpers/volume"
 	log "github.com/sirupsen/logrus"
@@ -17,6 +19,12 @@ func Serve(c *cli.Context) error {
 	}
 	volHandler := volume.NewHandler(volDriver)
 	log.WithField("command", "serve").Infof("listening on %s", c.String("socket"))
+	defer func() {
+		err := os.Remove(c.String("socket"))
+		if err != nil {
+			log.WithField("command", "serve").Warnf("couldn't remove socket: %s", c.String("socket"))
+		}
+	}()
 	log.Error(volHandler.ServeUnix(c.String("socket"), 0))
 	return nil
 }
